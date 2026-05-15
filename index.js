9952b8438b468677207e524f5072c08fe795a328
WGrout <wgrout87@gmail.com>
2026-05-15 17:36:09 -0600

    tested drawing image to canvas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function pageLoaded() {
-    alert('Hello World!');
-}
+var canvas = document.getElementById('testcanvas');
+var context = canvas.getContext('2d');
 
-window.onload = pageLoaded()
+var image = document.getElementById('spaceship');
+context.drawImage(image, 0, 350);
+context.drawImage(image, 0, 400, 100, 25);
+context.drawImage(image, 0, 0, 60, 50, 0, 420, 60, 50);
